04439c8824216c94ad2fe34e5f15aa1b03b70fd2
bluecheat <itsinil@gmail.com>
2023-07-18 09:34:21 +0900

    fix: http provider axios 대체 및 webpack 번들링 옵션 수정

--- src/rpcapi/provider/http.ts
+++ src/rpcapi/provider/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 function invalidResponseError(result, host) {
-	const message = !!result && !!result.error && !!result.error.message ? `[ethjs-provider-http] ${result.error.message}` : `[ethjs-provider-http] Invalid JSON RPC response from host provider ${host}: ${JSON.stringify(result, null, 2)}`;
+	const message = !!result && !!result.error && !!result.error.message ? `[axios] ${result.error.message}` : `[axios] Invalid JSON RPC response from host provider ${host}: ${JSON.stringify(result, null, 2)}`;
 	return new Error(message);
 }
 
@@ -11,11 +11,8 @@ class HttpProvider {
 	private timeout: number;
 
 	constructor(host, timeout = 0) {
-		if (!(this instanceof HttpProvider)) {
-			throw new Error('[ethjs-provider-http] the HttpProvider instance requires the "new" flag in order to function normally (e.g. `const eth = new Eth(new HttpProvider());`).');
-		}
 		if (typeof host !== 'string') {
-			throw new Error('[ethjs-provider-http] the HttpProvider instance requires that the host be specified (e.g. `new HttpProvider("http://localhost:8545")` or via service like infura `new HttpProvider("http://ropsten.infura.io")`)');
+			throw new Error('[http] the HttpProvider instance requires that the host be specified (e.g. `new HttpProvider("http://localhost:8545")` or via service like infura `new HttpProvider("http://ropsten.infura.io")`)');
 		}
 
 		this.host = host;
@@ -25,7 +22,8 @@ class HttpProvider {
 	sendAsync(payload, callback) {
 		const self = this;
 
-		axios.post(self.host, payload, {
+		return axios.post(self.host, payload, {
+			adapter: typeof window !== 'undefined' ? 'xhr' : 'http',
 			headers: {
 				'Content-Type': 'application/json',
 			},
@@ -36,7 +34,7 @@ class HttpProvider {
 				callback(null, data);
 			})
 			.catch((error) => {
-				let errorMessage = `[ethjs-provider-http] Error sending HTTP request: ${error.message}`;
+				let errorMessage = `[axios] Error sending HTTP request: ${error.message}`;
 				if (error.response && error.response.data) {
 					const { data } = error.response;
 					errorMessage = invalidResponseError(data, self.host).message;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 var path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
+const browserConfig = {
 	entry: path.resolve('./src/index.ts'),
 	module: {
 		rules: [
@@ -17,21 +16,53 @@ module.exports = {
 		extensions: ['.tsx', '.ts', '.js'],
 	},
 	devtool: 'cheap-module-source-map',
+	target: 'web',  // 브라우저 타겟 설정
 	output: {
 		library: 'SymWeb3',
 		libraryTarget: 'umd',
 		globalObject: 'this',
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'sym-web3.js',
+		filename: 'sym-web3.browser.js',  // 브라우저용 파일명 변경
 		libraryExport : "default",
-		auxiliaryComment : "symweb3js Comment",
-		clean: true,
+		auxiliaryComment : "symweb3js Comment for Browser",
 	},
 	optimization: {
 	},
 	plugins: [
 		new webpack.ProvidePlugin({
-			process: 'process/browser.js',
+			process: 'process/browser',
 		}),
 	],
 };
+
+const nodeConfig = {
+	entry: path.resolve('./src/index.ts'),
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	devtool: 'cheap-module-source-map',
+	target: 'node',  // 노드 타겟 설정
+	output: {
+		library: 'SymWeb3',
+		libraryTarget: 'umd',
+		globalObject: 'this',
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'sym-web3.js',  // 노드용 파일명 변경
+		libraryExport : "default",
+		auxiliaryComment : "symweb3js Comment for Node",
+	},
+	optimization: {
+	},
+	plugins: [],
+};
+
+module.exports = [browserConfig, nodeConfig];  // 두 설정 모두 내보내기
